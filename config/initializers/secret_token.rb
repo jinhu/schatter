fe3f2f70c1a836d3f31a68f3721796ff5a95c799
Jin <jin@hukelou.com>
2011-10-28 13:12:02 +0200

    rails skeleton with mysql db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schatter::Application.config.secret_token = '201c81b0abced1db0a771a3592c8cbd89e261d6e818ca76451de5d4e5d805b0828693657b872089a7e563a98df0f54bf1a9491c8e0dadc5c63e4727d58d79209'
